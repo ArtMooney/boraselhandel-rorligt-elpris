2ab7cf4bb67015d635cc3a265a7e0280295ea7bf
Henrik Skoglund <henrik.skoglund@framecore.se>
2023-06-16 11:28:38 +0200

    prices are now fetched and rendered

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,41 @@
+<style scoped>
+@import "./css/normalize.css";
+@import "./css/components.css";
+@import "./css/rorligt-elpris.css";
+</style>
+
 <template>
   <div class="rorligt-elpris-app">
-    <div
-      id="w-node-_051a9b96-4ead-e118-8a59-194af47cb073-cdc55134"
-      class="rorligt-elpris-wrapper"
-    >
+    <div class="rorligt-elpris-wrapper">
       <div
-        id="w-node-_0ce87386-09bd-15c0-b072-0835fb14c6cc-cdc55134"
-        class="elomrade left selected"
+        :class="[elomrade === 0 ? 'elomrade left selected' : 'elomrade left']"
       >
         <div class="elpris-rubrik">Elområde 1</div>
         <div class="elpris-wrapper">
-          <div class="rorligt-elpris">64,22</div>
+          <div class="rorligt-elpris">{{ pricesArea1.priceAverage }}</div>
           <div class="elpris-kwh">öre/kWh</div>
         </div>
       </div>
-      <div
-        id="w-node-_91f649a3-bb70-a795-b83a-2447edc0e2f4-cdc55134"
-        class="elomrade"
-      >
+      <div :class="[elomrade === 1 ? 'elomrade selected' : 'elomrade']">
         <div class="elpris-rubrik">Elområde 2</div>
         <div class="elpris-wrapper">
-          <div class="rorligt-elpris">64,22</div>
+          <div class="rorligt-elpris">{{ pricesArea2.priceAverage }}</div>
           <div class="elpris-kwh">öre/kWh</div>
         </div>
       </div>
-      <div
-        id="w-node-cddf50a4-4c92-fe6b-0172-49fec9704d62-cdc55134"
-        class="elomrade selected"
-      >
+      <div :class="[elomrade === 2 ? 'elomrade selected' : 'elomrade']">
         <div class="elpris-rubrik">Elområde 3</div>
         <div class="elpris-wrapper">
-          <div class="rorligt-elpris">66,50</div>
+          <div class="rorligt-elpris">{{ pricesArea3.priceAverage }}</div>
           <div class="elpris-kwh">öre/kWh</div>
         </div>
       </div>
       <div
-        id="w-node-_15d05f01-d5c1-4387-6d10-54988da43617-cdc55134"
-        class="elomrade right selected"
+        :class="[elomrade === 3 ? 'elomrade right selected' : 'elomrade right']"
       >
         <div class="elpris-rubrik">Elområde 4</div>
         <div class="elpris-wrapper">
-          <div class="rorligt-elpris">71,72</div>
+          <div class="rorligt-elpris">{{ pricesArea4.priceAverage }}</div>
           <div class="elpris-kwh">öre/kWh</div>
         </div>
       </div>
@@ -48,17 +43,96 @@
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      spotPeriodWebhook: "https://api.ngine.se/webhook/elhandel-getareas",
+      userName: "BorasElhandel",
+      userPass: "kjzsdnfkjdsf87324",
+      elomrade: null,
+      spotPeriodPrices: [],
+      pricesArea1: "-",
+      pricesArea2: "-",
+      pricesArea3: "-",
+      pricesArea4: "-",
+    };
+  },
+
+  async created() {
+    // this.spotPeriodPrices = await this.getCmsData(
+    //   `${
+    //     this.spotPeriodWebhook
+    //   }?searchstart=${this.getFirstDayOfCurrentMonth()}&searchstop=${this.getFormattedDate()}&searcharea=${3}`
+    // );
+
+    this.spotPeriodPrices = await this.getApiData(this.spotPeriodWebhook);
+    this.processPrices();
+  },
+
+  methods: {
+    getApiData(urlEndpoint) {
+      return new Promise((resolve, reject) => {
+        var requestOptions = {
+          method: "GET",
+          headers: {
+            Authorization: "Basic " + btoa(this.userName + ":" + this.userPass),
+          },
+          redirect: "follow",
+        };
+
+        fetch(urlEndpoint, requestOptions)
+          .then((response) => {
+            if (!response.ok) throw new Error();
+            return response.json();
+          })
+          .then((result) => {
+            console.log(result);
+            resolve(result);
+          })
+          .catch((error) => {
+            console.log(error);
+            reject(error);
+          });
+      });
+    },
+
+    getFormattedDate(dateInput) {
+      const date = !dateInput ? new Date() : dateInput;
+
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+
+      return `${year}-${month}-${day}`;
+    },
+
+    getFirstDayOfCurrentMonth() {
+      const currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth() + 1;
+
+      return `${year}-${String(month).padStart(2, "0")}-01`;
+    },
+
+    processPrices() {
+      for (const area of this.spotPeriodPrices) {
+        if (area.searchArea === "SE1") this.pricesArea1 = area;
+        if (area.searchArea === "SE2") this.pricesArea2 = area;
+        if (area.searchArea === "SE3") this.pricesArea3 = area;
+        if (area.searchArea === "SE4") this.pricesArea4 = area;
+      }
+    },
+
+    getCookie(name) {
+      var value = "; " + document.cookie;
+      var parts = value.split("; " + name + "=");
+      if (parts.length === 2) {
+        return parts.pop().split(";").shift();
+      }
+    },
+  },
+};
+</script>
