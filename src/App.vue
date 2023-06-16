9f0d49e4a622134dd5b922aedcf0333d3089b985
Henrik Skoglund <henrik.skoglund@framecore.se>
2023-06-16 07:56:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="rorligt-elpris-app">
+    <div
+      id="w-node-_051a9b96-4ead-e118-8a59-194af47cb073-cdc55134"
+      class="rorligt-elpris-wrapper"
+    >
+      <div
+        id="w-node-_0ce87386-09bd-15c0-b072-0835fb14c6cc-cdc55134"
+        class="elomrade left selected"
+      >
+        <div class="elpris-rubrik">Elområde 1</div>
+        <div class="elpris-wrapper">
+          <div class="rorligt-elpris">64,22</div>
+          <div class="elpris-kwh">öre/kWh</div>
+        </div>
+      </div>
+      <div
+        id="w-node-_91f649a3-bb70-a795-b83a-2447edc0e2f4-cdc55134"
+        class="elomrade"
+      >
+        <div class="elpris-rubrik">Elområde 2</div>
+        <div class="elpris-wrapper">
+          <div class="rorligt-elpris">64,22</div>
+          <div class="elpris-kwh">öre/kWh</div>
+        </div>
+      </div>
+      <div
+        id="w-node-cddf50a4-4c92-fe6b-0172-49fec9704d62-cdc55134"
+        class="elomrade selected"
+      >
+        <div class="elpris-rubrik">Elområde 3</div>
+        <div class="elpris-wrapper">
+          <div class="rorligt-elpris">66,50</div>
+          <div class="elpris-kwh">öre/kWh</div>
+        </div>
+      </div>
+      <div
+        id="w-node-_15d05f01-d5c1-4387-6d10-54988da43617-cdc55134"
+        class="elomrade right selected"
+      >
+        <div class="elpris-rubrik">Elområde 4</div>
+        <div class="elpris-wrapper">
+          <div class="rorligt-elpris">71,72</div>
+          <div class="elpris-kwh">öre/kWh</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
